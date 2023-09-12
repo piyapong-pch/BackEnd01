02f7a23e21d8d6e8b30a24138ebac04982879823
rainthyst <rain-piyapong@outlook.com>
2023-09-12 21:04:32 +0700

    new code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -20,8 +19,10 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get(`/api/:date?`, function (req, res) {
+  var date = new Date(req.params.date);
+  res.json({unix: date,
+  utc: date.toUTCString()})
 });
 
 
